01330d1068dad0c9331c956b0660596b0fd8a8de
paran3 <lemibi11@naver.com>
2023-01-15 00:47:45 +0900

    cors 이슈.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.v1.api import api_router
 from core.config import settings
@@ -12,7 +12,7 @@ app = FastAPI(
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, workers=1)
+    uvicorn.run(app, host="0.0.0.0", port=80, workers=1)
 
 origins = ["*"]
 
